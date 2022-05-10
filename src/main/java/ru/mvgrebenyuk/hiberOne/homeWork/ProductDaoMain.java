cc182c8808775ecaad11d07b81cae889b74c26d5
alekoChe <aleko2117bis@gmail.com>
2022-05-10 13:14:55 +0300

    удалил лишнее

--- src/main/java/ru/mvgrebenyuk/hiberOne/homeWork/ProductDaoMain.java
+++ src/main/java/ru/mvgrebenyuk/hiberOne/homeWork/ProductDaoMain.java
@@ -6,10 +6,7 @@ public class ProductDaoMain {
         SessionFactoryUtils sessionFactoryUtils = new SessionFactoryUtils();
         sessionFactoryUtils.init();
             try {
-                //UserDao userDao = new UserDaoImpl(sessionFactoryUtils);
                 ProductDao productDao = new ProductDaoImpl(sessionFactoryUtils);
-                //userDao.testCache();
-                //productDao.testCache();
                 System.out.println(productDao.findByName("Bread")); ////
             } catch (Exception e){
                 e.printStackTrace();
